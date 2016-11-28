6d309df3c4731c8d9125992ae97de460dab8b99c
Ava Lakmazaheri <alakmazaheri@olin.edu>
2016-11-28 15:39:48 -0500

    updated

--- First Pass/plotpath.m
+++ First Pass/plotpath.m
@@ -1,32 +1,38 @@
 function res = plotpath(F_app)
-    final_time = 30;    
+    close all
+    final_time = 10*60;    
     theta0 = 0;
     omega0 = 0;
-    d = [theta0; omega0];
+    d = [theta0; omega0; F_app];
     
-    [t,Out] = ode45(@diffeqs, [0 final_time], d);
+    [t,Out] = ode45(@diffeqs, [0:1:final_time], d);
     
-    theta = Out(:,1);
-    omega = Out(:,2);
+    theta = Out(:,1)*1e4;
+    disp(length(theta))
+    omega = Out(:,2)*1e4;
+    T = Out(:,3);
     P1 = [0 0];
     
     figure
     axis(gca, 'equal');
-    axis([-3 3 -3 3]);
+    xlim([0 final_time]);
+    axis([-3 3 -3 3])
 
-    for t = 1:10
         for inc = 1:length(theta)
             curr_angle = theta(inc);
-
+            %curr_angle = t/10;
+            
             P2 = [cos(curr_angle) sin(curr_angle)];
             P3 = -1*[cos(curr_angle) sin(curr_angle)];
             top_half = line([P1(1) P2(1)], [P1(2) P2(2)]);
-            bottom_half = line([P1(1) P3(1)], [P1(2) P3(2)])
+            bottom_half = line([P1(1) P3(1)], [P1(2) P3(2)]);
             pause(0.01);
             delete(top_half);
             delete(bottom_half);
+            %log(t) = curr_angle;
         end
-    end
-    res = Out;
+   
+    plot(t, omega)
+    res = omega;
 end
 
